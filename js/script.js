bc480d533c9d73cde33ec3fd4bb169d7c702f169
Alessandro Hue <alessandroespinoza@lieovvh.cl>
2025-05-07 13:03:19 -0400

    aaaaa

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//Prueba de conexion JS
+console.log("conexion establecida con JS!");
+
+//creacion e variables
+let num= 25;
+let decimal=3.1416;
+let texto= "hola";
+let boolean= "verdadero_o_falso";
+//creacion de funcion numeroEntero, numero decimal
+
+function numeroEntero() {
+    return "Variable Númerica:" + num;
+}
+
+function numeroDeciaml() {
+    return "Variable decimal:" + decimal;
+}
+
+function cadena_Texto(){
+    return "variable texto:" + texto;
+}
+
+function valor_Booleano(){
+    return "variable boolean;"+boolean;
+}
+
+//impresion de nose
+console.log(num)
+console.log(decimal)
+console.log(texto)
+console.log(boolean)
+
