d9e1d13b9882fbf5921e6e4c79e004845ae9e966
amyra-abid <amyrasandoval@liceovvh.cl>
2025-05-16 12:47:16 -0400

    sss

---
+++ js/02_arrays/04_ejercicio/script.js
@@ -0,0 +1,16 @@
+console.log("conectando correctamente...");
+
+/*
+Enunciado:
+Crea una función que multiplique los elementos
+ubicados en posiciones pares (0, 2, 4) de un 
+arreglo. Muestra el resultado con un alert.
+
+*/
+
+function multiplicaPares() {
+    let numeros= [10,1,2,3,4,5];
+    let multiplica= ( numeros[4]);
+    alert("la multiplicacion de sus pociciones sera el resultado: \n"
+    +multiplica);
+}
