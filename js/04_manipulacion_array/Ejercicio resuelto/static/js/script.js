8dd80ace29b6f619be83f27237001bfe1ed7333a
Alessandro-hue <alessandroespinoza@liceovvh.cl>
2025-06-06 13:01:11 -0400

    ssdd

--- js/04_manipulacion_array/Ejercicio resuelto/static/js/script.js
+++ js/04_manipulacion_array/Ejercicio resuelto/static/js/script.js
@@ -11,44 +11,101 @@ function listaCompras(){
 
 //Ejercicio 2 
 
-function eliminarElemento(){
-    let compraEliminada= compras.pop();
-    alert(`Elemento eliminado: ${compraEliminada}`);
+function eliminarcompras(){
+    //mostrar lista de compras eleminadas
+    alert(`lista de compras${compras}.`)
+    //guardar el elemento eleminado en una variable
+    let eliminado = compras.pop();
+    //mostrar la lista actualizada
+    alert(`Producto eleminado ${eliminado}\nlista de compra actualizada: ${compras}.`);
+
 }
 
 //ejercicio 3
-let colores=[];
-function listaColores(){
-    colores.unshift("rojo", "azul");
-    let colores= (`amarillo`);
-    alert(`los nuevos colores: ${colores.join(" - ")}`);
+let colores =[ "rojo", "azul"];
+
+function agregarcolor(){
+    let entrada = prompt("Agregar un color");
+    colores.unshift(entrada);
+    alert(`el color agregado es ${entrada}\n colores actualizados: ${colores}`);
 }
+
 
 //ejercicio 4
+function eliminarPrimercolor(){
+    let coloreleiminado = colores.shift();
+    alert("el color fue eleminado - " + coloreleiminado + 
+        "\n colores actualizados" )
+
+}
+
+//ejercicio 5
+/* Ejercicio 5: Combinación de métodos
+Crea un arreglo llamado numeros = [10, 20, 30].
+Agrega 40 al final.
+Quita el primer número.
+Agrega 5 al inicio.
+Elimina el último número.
+Muestra el arreglo final usando console.log().*/
+
+let numeros=[10,20,30]
+function agregarNumeros(){
+    numeros.push(40)
+    alert(`Le agregamos 40 al final`);
+    numeros.shift()
+    alert(`le quitamos el primer numero`);
+    numeros.unshift(5)
+    alert(`le agregamos un 5 al final`);
+    numeros.pop()
+    alert(`eliminamos el ultimo numero`);
+    alert(`El arreglo quedo asi $(numeros.joing(-->"))`)
+    
+}
+//ejecicio 6
+
+let orden=[]
+
+function agregarOrdenInverso(){
+    let num1 = prompt("ingrese el numero 1");
+    orden.unshift(num1)
+    let num2 = prompt("ingrese el numero 2");
+    orden.unshift(num2)
+    let num3 = prompt("ingrese el numero 3");
+    orden.unshift(num3);
+
+    console.log(`orden Inverso: ${orden.join(" --> ")}`);
 
 
-// Creación de Arreglo
-let array = ["Xbox","Nintendo"];
-//Creación de funciones
-function usarPush(){
-// Agregar al final
-    let entrada = prompt("Agregar una consola de juego: ");
-    array.push(entrada);
-    alert("El nuevo arreglo es: " + array.join(" - "));
 }
-function usarPop(){
-    //Elimina el último elemento
-    let basurero = array.pop();
-    alert(`Elemento eliminado: ${basurero}`);
+
+
+//ejercicio 7
+let historial = [];
+
+function historialMensaje(){
+    historial.push("Hola","¿como estas?", "adios.");
+    let eliminado = historial.pop();
+    alert(`ultimo mensaje eliminado: ${eliminado}`);
+    alert(`Historial de mensaje actualizado:${historial.join(", ")}`)
 }
-function usarUnshift(){
-    // Agregar al inicio
-    let entrada = prompt("Agregar una consola de juego: ");
-    array.unshift(entrada);
-    alert("El nuevo arreglo es: " + array.join(" "));
+
+
+
+
+
+
+
+//ejercicio 8
+let cola =[" cliente1", "cliente2", "cliente3"]
+
+function actualizarCola(){
+    cola.shift();
+    cola.push("cliente4");
+    alert("cola: "+ Cola);
 }
-function usarShift(){
-        //Elimina el primer elemento
-        let basurero = array.shift();
-        alert(`Elemento eliminado: ${basurero}`);
-}
+
+
+
+
+
+
