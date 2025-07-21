b533c6a224d85a76d5ee851323b26fa439409e7f
Alessandro-hue <alessandroespinoza@liceovvh.cl>
2025-07-21 13:00:18 -0400

    tttt

--- js/07_Uso_funciones_return/static/js/script.js
+++ js/07_Uso_funciones_return/static/js/script.js
@@ -7,23 +7,23 @@ y retorne el resultado completo como una
 cadena de texto. Muestra el resultado con
   alert. */
 
-function repetirPalabra(palabra, veces){
-    let resultado = "";
-    for(let i = 0; i < veces; i++){
-        resultado += `${palabra} `//  'hola hola hola hola '
-    }
-    return resultado.trim(); // trim elimina espacios 'hola hola hola hola'
-    // en los extremos
+function repetirPalabra(palabra, veces) {
+  let resultado = "";
+  for (let i = 0; i < veces; i++) {
+    resultado += `${palabra} `//  'hola hola hola hola '
+  }
+  return resultado.trim(); // trim elimina espacios 'hola hola hola hola'
+  // en los extremos
 }
 
-function mostrarPalabras(){
-let input1 = document.getElementById("input1");
-let palabra = input1.value;
-let numero = parseInt(prompt("Ingrese cantidad a repetir"));
+function mostrarPalabras() {
+  let input1 = document.getElementById("input1");
+  let palabra = input1.value;
+  let numero = parseInt(prompt("Ingrese cantidad a repetir"));
 
-let resultadoFinal = repetirPalabra(palabra, numero);
+  let resultadoFinal = repetirPalabra(palabra, numero);
 
-document.getElementById("resultado1").textContent = resultadoFinal;
+  document.getElementById("resultado1").textContent = resultadoFinal;
 }
 
 /*🟦 Ejercicio 2: Comparar dos números
@@ -32,32 +32,32 @@ los valores y retorne un mensaje indicando cuál número es mayor, menor o si so
 */
 
 
-function compararNumeros(num1, num2){
-    if(num1 > num2){
-        return `${num1} es mayor que ${num2}`;
-    } else if(num1 < num2){
-        return `${num1} es menor que ${num2}`;
-    } else {
-        return `${num1} es igual a ${num2}`;
-    }
+function compararNumeros(num1, num2) {
+  if (num1 > num2) {
+    return `${num1} es mayor que ${num2}`;
+  } else if (num1 < num2) {
+    return `${num1} es menor que ${num2}`;
+  } else {
+    return `${num1} es igual a ${num2}`;
+  }
 }
 
-function mostrarComparacion(){
-    let input1 = document.getElementById("numComp1");
-    let input2 = document.getElementById("numComp2")
-    let num1 = parseInt(input1.value);
-    let num2 = parseInt(prompt("Ingrese otro número"));
-    let resultado = compararNumeros(num1, num2);
-    document.getElementById("resultado2").textContent = resultado;
+function mostrarComparacion() {
+  let input1 = document.getElementById("numComp1");
+  let input2 = document.getElementById("numComp2")
+  let num1 = parseFloat(input1.value);
+  let num2 = parseFloat(input2.value);
+  let resultado = compararNumeros(num1, num2);
+  document.getElementById("resultado2").textContent = resultado;
 
-    if (isNaN(num1) || isNaN(num2)) {
-        alert("valor invalido, ingrese solo numeros");
+  if (isNaN(num1) || isNaN(num2)) {
+    alert("valor invalido, ingrese solo numeros");
 
-    }
-      input1.value = ""; // Limpiar la caja de texto
-      input1.focus() ; //
-      input2.value ="";
   }
+  input1.value = ""; // Limpiar la caja de texto
+  input1.focus(); //
+  input2.value = "";
+}
 
 /*🟦 Ejercicio 3: Verificar si un número es par o impar
 📌 Solicita un número usando input. Diseña una función que reciba ese número como parámetro, determine si es par o impar, 
@@ -65,18 +65,53 @@ y retorne un mensaje con el resultado. Muestra ese mensaje al usuario  en html *
 
 
 
-function vereficarParImpar(numero){
-    if(numero % 2 === 0){
-        return `${numero} es un número par`;
-    } else {
-        return `${numero} es un número impar`;
-    }
-} 
-
-function mostrarParImpar(){
-    let input3 = document.getElementById("input3");
-    let numero = parseInt(input3.value);
-    let resultadoParImpar = vereficarParImpar(numero);
-    document.getElementById("resultado3").textContent = resultadoParImpar;
+function vereficarParImpar(numero) {
+  if (numero % 2 === 0) {
+    return `${numero} es un número par`;
+  } else {
+    return `${numero} es un número impar`;
+  }
 }
 
+function Pares() {
+  const inputNum = document.getElementById("input3");
+  let num = parseInt(inputNum.value);
+  if (isNaN(num) || num === 0); {
+    document.getElementById("resultado3").textContent =
+      "Valor invalido, Por favor ingrese un número válidos.";
+      return;
+  }
+}
+
+/*
+🟦 Ejercicio 4: Validar si un número es primo
+📌 Captura un número usando input. Luego, crea una función que reciba
+ese número como parámetro y retorne un mensaje indicando si el número es primo o no. 
+Muestra el resultado con 
+html*/
+
+
+function esPrimo(numeroPrimo) {
+  if (numeroPrimo <= 1){
+      return `El numero ${numeroPrimo} no es primo`
+  }
+  for (let i = 2; i < numeroPrimo; i++){
+      if (numeroPrimo % i === 0)
+          return `El numero ${numeroPrimo} no es primo`
+  }
+  return `El número ${numeroPrimo} es primo`;
+}
+
+
+function verificarNumeroPrimo() {
+  const inputPrimo = document.getElementById("numeroPrimo");
+  const numeroPrimo = parseInt(inputPrimo.value);
+  if(isNaN(numeroPrimo) || numeroPrimo <= 0){
+      document.getElementById("resultado4").innerText = "Entrada inválida. ingrese un numero o un número mayor a 0.";
+      return;
+  }
+  resultado = esPrimo(numeroPrimo)
+  document.getElementById("resultado4").innerText = `${resultado}`;
+  inputPrimo.value = ""
+  inputPrimo.focus()
+}
