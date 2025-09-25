19e096f4ca998bb54ae96ab3948f491b210405ce
alessandro-hue <alessandroespinoza@liceovvh.cl>
2025-09-25 09:08:53 -0300

    yii

---
+++ js/11_tarea_nueva(onclick)/static/js/main.js
@@ -0,0 +1,14 @@
+function example(elemonto){
+    console.log("elemonto clikeado", elemento);
+}
+
+function turnoOFF(elemento){
+    if(elemento.innerTEXT == "of"){
+        elemento.innerTEXT = "on"
+    }else{
+        elemento.innerTEXT = "of"
+    }
+}
+function renove(element){
+    element.renove();
+}
